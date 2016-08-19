9c3370f49094f1f16e3cc2ebc16a17e7bc1f9195
Natsumi Hoshino <Natsumi Hoshino>
2016-08-20 05:09:42 +0900

    it wasn't that simple, setting up google login

--- config/accounts.js
+++ config/accounts.js
@@ -1,4 +1,4 @@
-const passwordField = AccountsTemplates.removeField('password');
+/*const passwordField = AccountsTemplates.removeField('password');
 const emailField = AccountsTemplates.removeField('email');
 AccountsTemplates.addFields([{
   _id: 'username',
@@ -6,53 +6,36 @@ AccountsTemplates.addFields([{
   displayName: 'username',
   required: true,
   minLength: 2,
-}, emailField, passwordField]);
-
+}, emailField, passwordField]);*/
+// Only allow festivaljapon logins
+Accounts.config({ restrictCreationByEmailDomain: 'festivaljapon.com'});
 AccountsTemplates.configure({
   defaultLayout: 'userFormsLayout',
   defaultContentRegion: 'content',
   confirmPassword: false,
-  enablePasswordChange: true,
-  sendVerificationEmail: true,
-  showForgotPasswordLink: true,
-  onLogoutHook() {
+  enablePasswordChange: false,
+  sendVerificationEmail: false,
+  showForgotPasswordLink: false,
+  /*onLogoutHook() {
     const homePage = 'home';
     if (FlowRouter.getRouteName() === homePage) {
       FlowRouter.reload();
     } else {
       FlowRouter.go(homePage);
     }
-  },
+  },*/
 });
 
-['signIn', 'signUp', 'resetPwd', 'forgotPwd', 'enrollAccount'].forEach(
+/*['signIn', 'signUp', 'enrollAccount'].forEach(
   (routeName) => AccountsTemplates.configureRoute(routeName));
 
-// We display the form to change the password in a popup window that already
-// have a title, so we unset the title automatically displayed by useraccounts.
-AccountsTemplates.configure({
-  texts: {
-    title: {
-      changePwd: '',
-    },
-  },
-});
-
-AccountsTemplates.configureRoute('changePwd', {
-  redirect() {
-    // XXX We should emit a notification once we have a notification system.
-    // Currently the user has no indication that his modification has been
-    // applied.
-    Popup.back();
-  },
-});
 
 if (Meteor.isServer) {
   if (process.env.MAIL_FROM) {
     Accounts.emailTemplates.from = process.env.MAIL_FROM;
   }
 
-  ['resetPassword-subject', 'resetPassword-text', 'verifyEmail-subject', 'verifyEmail-text', 'enrollAccount-subject', 'enrollAccount-text'].forEach((str) => {
+  ['enrollAccount-subject', 'enrollAccount-text'].forEach((str) => {
     const [templateName, field] = str.split('-');
     Accounts.emailTemplates[templateName][field] = (user, url) => {
       return TAPi18n.__(`email-${str}`, {
@@ -62,4 +45,4 @@ if (Meteor.isServer) {
       }, user.getLanguage());
     };
   });
-}
+}*/
