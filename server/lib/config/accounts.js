3e9276985b08cc5efd973b920dbe9e4643748860
Natsumi Hoshino <Natsumi Hoshino>
2016-08-20 17:26:24 +0900

    moving stuff to server?

--- server/lib/config/accounts.js
+++ server/lib/config/accounts.js
@@ -9,5 +9,23 @@ Meteor.startup(function() {
     },
     { upsert: true }
   );
+  // Only allow festivaljapon logins
+  Accounts.config({ restrictCreationByEmailDomain: 'festivaljapon.com'});
 
+  Accounts.onCreateUser((options, user) => {
+    if (! user.services.google) {
+      throw new Error('Expected login with Google only.');
+    }
+    const { picture, email, name } = user.services.google;
+    user.profile.fullname = name;
+    user.profile.emailBuffer = [email];
+    user.profile.avatarUrl = picture;
+    user.username = email;
+
+  //const { first_name, last_name } = user.services.facebook;
+  //user.initials = first_name[0].toUpperCase() + last_name[0].toUpperCase();
+
+  // Don't forget to return the new user object at the end!
+    return user;
+  });
 });
